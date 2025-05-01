f67aab118b9ca3dc542de8da0d557ce86758cb30
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-05-01 20:50:59 +0600

    1-5 Object , Optional and Literal Types

---
+++ module1/src/1.5-object.ts
@@ -0,0 +1,17 @@
+// :::::: Reference Type - Object ::::::::
+const user : {
+  readonly company: string; //Literal Type
+  firstName : string;
+  middleName?: string;  //Optional Type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "Programming Hero",
+  firstName : "Mithun",
+  middleName : "Kumer",
+  lastName : "Modak",
+  isMarried: true,
+}
+
+
+
