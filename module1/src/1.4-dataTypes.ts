6bd764c1e40f1fd60351f25f5e4342c53a722081
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-05-01 16:51:00 +0600

    1-4 Basic Data Types

---
+++ module1/src/1.4-dataTypes.ts
@@ -0,0 +1,30 @@
+// :::::: basic data types :::::::::
+
+// string
+let firstName : string = "Mithun";
+
+// number
+let roll : number = 97;
+
+// boolean
+let isAdmin : boolean = true;
+
+// undefined
+let x : undefined = undefined;
+
+// null
+let y : null = null;
+
+// declare variable without assigning value
+let d : number;
+
+// array
+let friendsName : string[] = ['Rakib', 'Probash']
+let eligibleRoll : number[] = [1, 2, 3]
+
+// tuple
+let coordinates : [number, number] = [3, 5];
+let aboutAdmin : [string, number, boolean] = ['Modak', 35, true];
+
+
+
