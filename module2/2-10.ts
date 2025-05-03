94d9fadef2e95c43e5b12a68979938534c14d1ad
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2025-05-03 22:50:56 +0600

    2-10: Mapped types

---
+++ module2/2-10.ts
@@ -0,0 +1,67 @@
+{
+  // :::: 2-10: Mapped types ::::
+
+  // :: review map method ::
+  const arrayOfNumbers: number[] = [1, 2, 3, 4, 5];
+
+  // const arrayOfStrings: string[] = ['1', '2', '3', '4', '5'];
+  const arrayOfStrings: string[] = arrayOfNumbers.map((num) => num.toString());
+  console.log(arrayOfStrings)
+
+
+  // :: map type ::
+  type AreaNumber ={
+    height: number;
+    width: number
+  }
+
+  //lookup type
+  type Height = AreaNumber['height'];       
+
+  // type AreaString ={
+  //   height: string;
+  //   width: string
+  // }
+
+
+  //map type  
+  type AreaString ={
+    [key in keyof AreaNumber]: string       
+  }
+
+ 
+  //map type  dynamically
+  type AreaStringDynamic<T> ={
+    [key in keyof T]: T[key]        
+  }
+
+  const area1 : AreaStringDynamic<{height: string; width: number}> = {
+    height: '100',
+    width: 200
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // 
+}
